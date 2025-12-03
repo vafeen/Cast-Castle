b936547256cc9a57a56ee625cfdf980e9f01af57
A <666av6@gmail.com>
2025-12-03 03:55:27 +0300

    update gradle configurations for publishing

--- cast-castle-processor/build.gradle.kts
+++ cast-castle-processor/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.com.vanniktech.maven.publish)
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -14,4 +15,41 @@ kotlin {
 dependencies {
     implementation(project(":cast-castle-annotations"))
     implementation(libs.symbol.processing.api)
+}
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    coordinates(
+        project.findProperty("group").toString(),
+        "cast-castle-processor",
+        project.findProperty("version").toString()
+    )
+
+    pom {
+        name.set("Cast Castle Processor")
+        description.set("Cast Castle Processor")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vafeen/CastCastle")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("vafeen")
+                name.set("A")
+                url.set("https://github.com/vafeen/")
+            }
+        }
+        scm {
+            url.set("https://github.com/vafeen/CastCastle")
+            connection.set("scm:git:git://github.com/vafeen/CastCastle.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vafeen/CastCastle.git")
+        }
+    }
 }
