ae87bbfb7c438455381c7f05e5e8d8ad2b1cdcec
A <666av6@gmail.com>
2025-12-03 03:57:10 +0300

    Publishing (#10)

--- cast-castle-annotations/build.gradle.kts
+++ cast-castle-annotations/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.com.vanniktech.maven.publish)
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -9,5 +10,42 @@ java {
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    coordinates(
+        project.findProperty("group").toString(),
+        "castcastle-annotations",
+        project.findProperty("version").toString()
+    )
+
+    pom {
+        name.set("Cast Castle Annotations")
+        description.set("Cast Castle Annotations")
+        inceptionYear.set("2025")
+        url.set("https://github.com/vafeen/CastCastle")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("vafeen")
+                name.set("A")
+                url.set("https://github.com/vafeen/")
+            }
+        }
+        scm {
+            url.set("https://github.com/vafeen/CastCastle")
+            connection.set("scm:git:git://github.com/vafeen/CastCastle.git")
+            developerConnection.set("scm:git:ssh://git@github.com/vafeen/CastCastle.git")
+        }
     }
 }
